09b99602c6835e6efe22f4f700c954d55b0895f8
7mw62sm6c9-cpu <7mw62sm6c9@privaterelay.appleid.com>
2026-09-04 15:18:54 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://trddcmyyhyanspandlhk.supabase.co";
+const SUPABASE_URL = "https://trdcdmyyhyanspandlhk.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_Ohcw_Kc0lyienmiK9wxFoQ_XtYaZjI2";
 
 const loginBox = document.getElementById("loginBox");
